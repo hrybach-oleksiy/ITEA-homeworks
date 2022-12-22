013053d0408f63aeee44a45124de5be0b7495702
hrybach-oleksiy <a.a.gribach@gmail.com>
2022-12-22 20:38:07 +0200

    feat: finish HW6

--- js/HW6/js/task5.js
+++ js/HW6/js/task5.js
@@ -1,57 +1,127 @@
-// 5. Для попереднього завдання створіть функцію, яка прийматиме масив об'єктів-співробітників, і кожному з об'єктів буде додавати нову властивість "salary", що зберігає зарплату співробітника.
-//    Зарплата розраховується, виходячи із значення властивості "occupation" таким чином:
-//      • "director" - 3000;
-//      • "manager" - 1500;
-//      • "programmer" - 2000;
-//      • для решти значень – 1000.
-//    Після виконання функції – вивести інформацію про співробітників.
-
-
-const employees = [
+const watchList = [
     {
-        "name": "Alex",
-        "sName": "Smith",
-        "age": 36,
-        "occupation": "manager"
+        "Title": "Inception",
+        "Year": "2010",
+        "Rated": "PG-13",
+        "Released": "16 Jul 2010",
+        "Runtime": "148 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+        "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+        "Language": "English, Japanese, French",
+        "Country": "USA, UK",
+        "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        "Metascore": "74",
+        "imdbRating": "8.8",
+        "imdbVotes": "1,446,708",
+        "imdbID": "tt1375666",
+        "Type": "movie",
+        "Response": "True"
     },
     {
-        "name": "Bob",
-        "sName": "Tornthon",
-        "age": 62,
-        "occupation": "director"
+        "Title": "Interstellar",
+        "Year": "2014",
+        "Rated": "PG-13",
+        "Released": "07 Nov 2014",
+        "Runtime": "169 min",
+        "Genre": "Adventure, Drama, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan, Christopher Nolan",
+        "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "Language": "English",
+        "Country": "USA, UK",
+        "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+        "Metascore": "74",
+        "imdbRating": "8.6",
+        "imdbVotes": "910,366",
+        "imdbID": "tt0816692",
+        "Type": "movie",
+        "Response": "True"
     },
     {
-        "name": "Jack",
-        "sName": "Grealish",
-        "age": 27,
-        "occupation": "programmer"
+        "Title": "The Dark Knight",
+        "Year": "2008",
+        "Rated": "PG-13",
+        "Released": "18 Jul 2008",
+        "Runtime": "152 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+        "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        "Language": "English, Mandarin",
+        "Country": "USA, UK",
+        "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+        "Metascore": "82",
+        "imdbRating": "9.0",
+        "imdbVotes": "1,652,832",
+        "imdbID": "tt0468569",
+        "Type": "movie",
+        "Response": "True"
     },
     {
-        "name": "Pep",
-        "sName": "Guardiola",
-        "age": 53,
-        "occupation": "coach"
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "Rated": "PG-13",
+        "Released": "15 Jun 2005",
+        "Runtime": "140 min",
+        "Genre": "Action, Adventure",
+        "Director": "Christopher Nolan",
+        "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+        "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+        "Language": "English, Urdu, Mandarin",
+        "Country": "USA, UK",
+        "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+        "Metascore": "70",
+        "imdbRating": "8.3",
+        "imdbVotes": "972,584",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Response": "True"
+    },
+    {
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rated": "PG-13",
+        "Released": "18 Dec 2009",
+        "Runtime": "162 min",
+        "Genre": "Action, Adventure, Fantasy",
+        "Director": "James Cameron",
+        "Writer": "James Cameron",
+        "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        "Language": "English, Spanish",
+        "Country": "USA, UK",
+        "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+        "Metascore": "83",
+        "imdbRating": "7.9",
+        "imdbVotes": "876,575",
+        "imdbID": "tt0499549",
+        "Type": "movie",
+        "Response": "True"
     }
 ];
 
-const addSalary = employees => {
-    for (let i = 0; i < employees.length; i++) {
-        switch (employees[i].occupation.toLowerCase()) {
-            case "director":
-                employees[i].salary = 3000;
-                break;
-            case "manager":
-                employees[i].salary = 1500;
-                break;
-            case "programmer":
-                employees[i].salary = 2000;
-                break;
-            default: employees[i].salary = 1000;
-        }
-    }
-    return employees;
+// 5. Масив watchList містить об'єкти з інформацією про кілька фільмів. Використовуйте filter і map для повернення нового масиву об'єктів тільки з title і rating, але де imdbRating більше або дорівнює 8.0.
+//         Результат повинен бути таким же як і в 4-му завданні - масив об'єктів на 2 поля, але відфільтрований, щоб рейтинг був більшим або дорівнює 8.0
+
+const filterFilms = films => {
+    return films.filter(film => +film.imdbRating >= 8)
+        .map(filteredFilm => (
+            {
+                title: filteredFilm.Title,
+                imdbRating: filteredFilm.imdbRating
+            }
+        ));
 };
 
-addSalary(employees);
-
-console.log(employees);
+console.log(filterFilms(watchList));
--- js/HW6/js/task6.js
+++ js/HW6/js/task6.js
@@ -1,54 +1,126 @@
 
-// sort(function(a, b) { return a > b });
-// 1, 2, 3, 5, 10, 14
-// 1, 10, 14, 2, 3,
-
-
-//    6. Для завдання 3 створити метод, що дозволяє відсортувати масив співробітників за однією з властивостей: name, sName, age, occupation, salary.
-//    Параметр сортування приймається від користувача.
-//    Після виконання функції – вивести інформацію про співробітників.
-
-const employees = [
+const watchList = [
     {
-        "name": "Alex",
-        "sName": "Smith",
-        "age": 36,
-        "occupation": "manager",
-        "salary": 1500
+        "Title": "Inception",
+        "Year": "2010",
+        "Rated": "PG-13",
+        "Released": "16 Jul 2010",
+        "Runtime": "148 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+        "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+        "Language": "English, Japanese, French",
+        "Country": "USA, UK",
+        "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        "Metascore": "74",
+        "imdbRating": "8.8",
+        "imdbVotes": "1,446,708",
+        "imdbID": "tt1375666",
+        "Type": "movie",
+        "Response": "True"
     },
     {
-        "name": "Bob",
-        "sName": "Tornthon",
-        "age": 62,
-        "occupation": "director",
-        "salary": 3000
+        "Title": "Interstellar",
+        "Year": "2014",
+        "Rated": "PG-13",
+        "Released": "07 Nov 2014",
+        "Runtime": "169 min",
+        "Genre": "Adventure, Drama, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan, Christopher Nolan",
+        "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "Language": "English",
+        "Country": "USA, UK",
+        "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+        "Metascore": "74",
+        "imdbRating": "8.6",
+        "imdbVotes": "910,366",
+        "imdbID": "tt0816692",
+        "Type": "movie",
+        "Response": "True"
     },
     {
-        "name": "Jack",
-        "sName": "Grealish",
-        "age": 27,
-        "occupation": "programmer",
-        "salary": 2000
+        "Title": "The Dark Knight",
+        "Year": "2008",
+        "Rated": "PG-13",
+        "Released": "18 Jul 2008",
+        "Runtime": "152 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+        "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        "Language": "English, Mandarin",
+        "Country": "USA, UK",
+        "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+        "Metascore": "82",
+        "imdbRating": "9.0",
+        "imdbVotes": "1,652,832",
+        "imdbID": "tt0468569",
+        "Type": "movie",
+        "Response": "True"
     },
     {
-        "name": "Pep",
-        "sName": "Guardiola",
-        "age": 53,
-        "occupation": "coach",
-        "salary": 1000
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "Rated": "PG-13",
+        "Released": "15 Jun 2005",
+        "Runtime": "140 min",
+        "Genre": "Action, Adventure",
+        "Director": "Christopher Nolan",
+        "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+        "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+        "Language": "English, Urdu, Mandarin",
+        "Country": "USA, UK",
+        "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+        "Metascore": "70",
+        "imdbRating": "8.3",
+        "imdbVotes": "972,584",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Response": "True"
+    },
+    {
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rated": "PG-13",
+        "Released": "18 Dec 2009",
+        "Runtime": "162 min",
+        "Genre": "Action, Adventure, Fantasy",
+        "Director": "James Cameron",
+        "Writer": "James Cameron",
+        "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        "Language": "English, Spanish",
+        "Country": "USA, UK",
+        "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+        "Metascore": "83",
+        "imdbRating": "7.9",
+        "imdbVotes": "876,575",
+        "imdbID": "tt0499549",
+        "Type": "movie",
+        "Response": "True"
     }
 ];
 
+// 6. Масив watchList містить об'єкти з інформацією про кілька фільмів. Використовуйте reduce, щоб знайти середній рейтинг IMDB фільмів, знятих режисером Крістофером Ноланом. Згадайте з попередніх завдань filter та map. Вам може знадобитися створити інші змінні, але збережіть остаточне середнє значення змінної AverageRating. Зверніть увагу, що значення рейтингу зберігаються у вигляді рядків в об'єкті і мають бути перетворені на числа, перш ніж вони будуть використані в будь-яких математичних операціях.
 
-const sortEmployees = (employees, property) => {
-    return employees.sort((a, b) => a[property] > b[property] ? 1 : -1);
+const getAvarageRating = films => {
+    const avargeRating = films.filter(film => film.Director === 'Christopher Nolan')
+        .reduce((acc, currValue, index, arr) => {
+            return acc + Number(currValue.imdbRating) / arr.length;
+        }, 0);
+
+    return avargeRating.toFixed(1);
 };
 
-const sortProperty = prompt('Enter property you want to sort by');
-
-sortEmployees(employees, sortProperty?.toLowerCase());
-
-employees.forEach((e) => {
-    console.log(`${e.name} ${e.sName} ${e.age} ${e.occupation} ${e.salary}`);
-});
-
+console.log(getAvarageRating(watchList));
