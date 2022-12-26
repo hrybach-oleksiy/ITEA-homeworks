625afd787892a3f47cdd7dfe18b952e158847858
hrybach-oleksiy <a.a.gribach@gmail.com>
2022-12-26 12:05:41 +0200

    feat: finish Task3, Task4

--- js/final_work/main.js
+++ js/final_work/main.js
@@ -1,8 +1,3 @@
-// 1. Прийняти від користувача імена людей для набору до команди.
-//    Імена прийняти та зберегти в новому масиві.
-//    Кількість імен має відповідати кількості посад.
-//    Масив з посадами - ['Junior developer', 'Middle developer', 'Senior developer', 'Junior QA',
-//    'Middle QA', 'Senior QA', 'Project manager'].
 
 //Task 1
 const positions = ['Junior developer', 'Middle developer', 'Senior developer', 'Junior QA', 'Middle QA', 'Senior QA', 'Project manager'];
@@ -29,9 +24,37 @@ for (let i = 0; i < employees.length; i++) {
     team[i + 1] = { name: employees[i], position: positions[i] };
 }
 
+//Task3-Task4
+const teamValues = Object.values(team);
+const getSalary = (minSalary, maxSalary) => {
+    return Math.floor(Math.random() * (maxSalary - minSalary) + minSalary);
+};
 
+for (let i = 0; i < teamValues.length; i++) {
+    const position = teamValues[i].position.toLowerCase();
+    if (position.indexOf('junior') === 0) {
+        teamValues[i].salary = getSalary(500, 1000);
+    } else if (position.indexOf('middle') === 0) {
+        teamValues[i].salary = getSalary(1500, 2000);
+    } else if (position.indexOf('senior') === 0) {
+        teamValues[i].salary = getSalary(2500, 3000);
+    } else {
+        teamValues[i].salary = getSalary(4000, 4500);
+    }
 
+    teamValues[i].tellAboutYourSelf = function () {
+        console.log(`My name is ${this.name} and I am a ${this.position}. I have a salary ${this.salary}$.`);
+    };
+}
 
+//Task5
+team.showTeam = function () {
+    for (let e in this) {
+        this[e].tellAboutYourSelf();
+    }
+};
+
+team.showTeam();
 
 
 
